dac90766772ffc398b430ec72b4ccf6a807494bf
Richard Hess <eswat2@gmail.com>
2020-04-06 15:54:35 -0700

    add lite version of bootstrap.css with unused css removed

--- src/App.svelte
+++ src/App.svelte
@@ -60,9 +60,7 @@
 </style>
 
 <svelte:head>
-  <link
-    rel="stylesheet"
-    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="/bootstrap.css" />
 </svelte:head>
 
 <main>
